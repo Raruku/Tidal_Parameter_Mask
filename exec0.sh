d628a0e2d88f2c39d99cb6af7697539a740d5fe9
pdn4kd <pdn4kd@mail.umkc.edu>
2015-09-29 16:23:41 -0500

    Unified rename/PSF download automation

--- exec0.sh
+++ exec0.sh
@@ -1,8 +1,5 @@
-perl SDSS_Rename_DR7.pl
-wget -i sdss-wget-PSF_DR7.lis
-chmod 755 Rename_PSF_DR7.sh
-./Rename_PSF_DR7.sh
-perl SDSS_Rename_S82.pl
-wget -i sdss-wget-PSF_S82.lis
-chmod 755 Rename_PSF_S82.sh
-./Rename_PSF_S82.sh
+perl SDSS_PSF.pl
+wget -i sdss-wget-PSF.lis
+chmod 755 Rename_PSF.sh
+./Rename_PSF.sh
+echo "run PSF_Cut.cl in IRAF"
--- exec1.sh
+++ exec1.sh
@@ -3,3 +3,4 @@ chmod +x aper_all.sh
 ./aper_all.sh
 perl APER_To_CSV.pl
 perl Single_SDSS_S82_Postage_Stamp.pl
+echo "Run Galaxy_cutouts.cl in IRAF"

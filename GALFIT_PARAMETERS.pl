0fe856f3afdbdd4f650b86b156bc7d33748b6df7
pdn4kd <pdn4kd@noreply.github.com>
2016-05-10 18:06:17 -0500

    better functionality for my purposes (1 fit per cutout)

--- GALFIT_PARAMETERS.pl
+++ GALFIT_PARAMETERS.pl
@@ -21,9 +21,11 @@ foreach my $DataRelease (@DataRelease) {
 	my @model_mag = map {$_->{'modelMag_r'}} @{$position_inputs};
 	my @modelErr_mag = map {$_->{'modelMagErr_r'}} @{$position_inputs};
 
-	open my $T_plot, '>', "Del_Parameters_$DataRelease.csv";
+	open my $T_plot_1, '>', "Del_Parameters_1_$DataRelease.csv";
+	open my $T_plot_2, '>', "Del_Parameters_2_$DataRelease.csv";
 
-	print $T_plot "ID_bulge,MAG_AUTO_bulge,MAGERR_AUTO_bulge,GAL_MAG_bulge,SDSS_Re_bulge,GAL_Re_bulge,Kron_bulge,Fit_Type_bulge,sersic_bulge,GAL_x_bulge,GAL_y_bulge,SEX_x_bulge,SEX_y_bulge,SEX_e_bulge,SEX_theta_bulge,GAL_e_bulge,GAL_theta_bulge,ID_disk,MAG_AUTO_disk,MAGERR_AUTO_disk,GAL_MAG_disk,SDSS_Re_disk,GAL_Re_disk,Kron_disk,Fit_Type_disk,sersic_disk,GAL_x_disk,GAL_y_disk,SEX_x_disk,SEX_y_disk,SEX_e_disk,SEX_theta_disk,GAL_e_disk,GAL_theta_disk\n";
+	print $T_plot_2 "ID_disk,MAG_AUTO_disk,MAGERR_AUTO_disk,GAL_MAG_disk,SDSS_Re_disk,GAL_Re_disk,Kron_disk,Fit_Type_disk,sersic_disk,GAL_x_disk,GAL_y_disk,SEX_x_disk,SEX_y_disk,SEX_e_disk,SEX_theta_disk,GAL_e_disk,GAL_theta_disk,ID_bulge,MAG_AUTO_bulge,MAGERR_AUTO_bulge,GAL_MAG_bulge,SDSS_Re_bulge,GAL_Re_bulge,Kron_bulge,Fit_Type_bulge,sersic_bulge,GAL_x_bulge,GAL_y_bulge,SEX_x_bulge,SEX_y_bulge,SEX_e_bulge,SEX_theta_bulge,GAL_e_bulge,GAL_theta_bulge,extra\n";
+	print $T_plot_1 "ID,MAG_AUTO,MAGERR_AUTO,GAL_MAG,SDSS_Re,GAL_Re,Kron,Fit_Type,sersic,GAL_x,GAL_y,SEX_x,SEX_y,SEX_e,SEX_theta,GAL_e,GAL_theta\n";
 
 	foreach my $posCount (0 .. scalar @nyuID - 1) {
 		foreach my $galaxy_fits (@galaxy_fits) {
@@ -34,6 +36,7 @@ foreach my $DataRelease (@DataRelease) {
 				$input_positions_1->column_names($input_positions_1->getline($file_in));
 				my $position_inputs_1 = $input_positions_1->getline_hr_all($file_in);
 
+				my @Type = map {$_->{'type'}} @{$position_inputs_1};
 				my @GAL_x = map {$_->{'x'}} @{$position_inputs_1};
 				my @GAL_y = map {$_->{'y'}} @{$position_inputs_1};
 				my @GAL_MAG = map {$_->{'Mag'}} @{$position_inputs_1};
@@ -58,16 +61,20 @@ foreach my $DataRelease (@DataRelease) {
 				foreach my $SEXCount (0 .. scalar @SEX_x - 1) {	
 					foreach my $gCount (0 .. scalar @GAL_x - 1) {	
 							if (($SEX_x[$SEXCount] <= $GAL_x[$gCount] + 5) && (($SEX_y[$SEXCount] <= $GAL_y[$gCount] + 5) && ($SEX_y[$SEXCount] >= $GAL_y[$gCount] - 5))
-							&& ($SEX_x[$SEXCount] >= $GAL_x[$gCount] - 5) && (($SEX_y[$SEXCount] <= $GAL_y[$gCount] + 5) && ($SEX_y[$SEXCount] >= $GAL_y[$gCount] - 5)) ) {
-									print $T_plot "${nyuID[$posCount]},$SEX_MAG[$SEXCount],$SEX_MAGErr[$SEXCount],$GAL_MAG[$gCount],$Re[$posCount],$GAL_Re[$gCount],$Kron[$SEXCount],$galaxy_fits,$SERSIC[$gCount],$GAL_x[$gCount],$GAL_y[$gCount],$SEX_x[$SEXCount],$SEX_y[$SEXCount],$SEX_e[$SEXCount],$SEX_theta[$SEXCount],$GAL_e[$gCount],$GAL_theta[$gCount],";
-							if (!($galaxy_fits eq "14") and !($galaxy_fits eq "aa")) {
-								print $T_plot ",,,,,,,,,,,,,,,";
+							&& ($SEX_x[$SEXCount] >= $GAL_x[$gCount] - 5) && (($SEX_y[$SEXCount] <= $GAL_y[$gCount] + 5) && ($SEX_y[$SEXCount] >= $GAL_y[$gCount] - 5)) && ($Type[$gCount] eq "sersic")) {
+									if (($galaxy_fits eq "14") or ($galaxy_fits eq "aa")) {
+										print $T_plot_2 "${nyuID[$posCount]},$SEX_MAG[$SEXCount],$SEX_MAGErr[$SEXCount],$GAL_MAG[$gCount],$Re[$posCount],$GAL_Re[$gCount],$Kron[$SEXCount],$galaxy_fits,$SERSIC[$gCount],$GAL_x[$gCount],$GAL_y[$gCount],$SEX_x[$SEXCount],$SEX_y[$SEXCount],$SEX_e[$SEXCount],$SEX_theta[$SEXCount],$GAL_e[$gCount],$GAL_theta[$gCount],";
+									} else {
+										print $T_plot_1 "${nyuID[$posCount]},$SEX_MAG[$SEXCount],$SEX_MAGErr[$SEXCount],$GAL_MAG[$gCount],$Re[$posCount],$GAL_Re[$gCount],$Kron[$SEXCount],$galaxy_fits,$SERSIC[$gCount],$GAL_x[$gCount],$GAL_y[$gCount],$SEX_x[$SEXCount],$SEX_y[$SEXCount],$SEX_e[$SEXCount],$SEX_theta[$SEXCount],$GAL_e[$gCount],$GAL_theta[$gCount]";
 								}
 							}
-						#last;
 						}
 					}
-				print $T_plot "\n";
+				if (($galaxy_fits eq "14") or ($galaxy_fits eq "aa")) {
+					print $T_plot_2 "\n";
+				} else {
+					print $T_plot_1 "\n";
+				}
 			}
 		}
 	}
